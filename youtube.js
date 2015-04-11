4531e4cac3f20466b2f88dd7d52fa06e09564fa7
ginpei <ginpei@gmail.com>
2015-04-11 14:48:19 -0700

    load YouTube script

--- youtube.js
+++ youtube.js
@@ -1,10 +1,77 @@
 (function() {
-	var elPlayer, videoId;
-	window.youtube = function(options) {
-		elPlayer = options.el;
-		videoId = options.id;
+	var Player = window.youtube = function(options) {
+		if (this instanceof Player) {
+			return this.initialize(options);
+		}
+		else {
+			var player = new Player(options);
+			// player.play();
+			return player;
+		}
 	};
 
+	/**
+	 * Load YoutTube API script.
+	 * Status is changed as: initial->loading->ready.
+	 * * Callback will run later if initial
+	 * * Callback is queued and will run if loading
+	 * * Callback run immediately if ready
+	 *
+	 * @param {Function} callback
+	 */
+	Player.loadYTScript = function(callback) {
+		var status = this._ytStatus;
+		if (status === undefined) {  // initial; not started
+			// initialize the callback queue
+			var callbacks = this._ytCallbacks = [];
+			callbacks.push(callback);
+
+			// load YoutTube script
+			var url = 'https://www.youtube.com/iframe_api';
+			var elScript = document.createElement('script');
+			elScript.src = url;
+			document.body.appendChild(elScript);
+
+			// set callbacks
+			window.onYouTubeIframeAPIReady = function() {
+				callbacks.forEach(function(callback, index) {
+					callback();
+				});
+				delete this._ytCallbacks;
+				this._ytStatus = 2;
+			}.bind(this);
+
+			// update status
+			this._ytStatus = 1;
+		}
+		else if (status === 1) {  // loading; started but not loaded yet
+			this._ytCallbacks.push(callback);
+		}
+		else if (status === 2) {  // ready; script is completely loaded
+			callback();
+		}
+	};
+
+	var $p = Player.prototype;
+
+	$p.initialize = function(options) {
+		this.el = options.el;
+		this.videoId = options.id;
+
+		this._loadYTScript(function() {
+			console.log('ready', this.videoId);
+		}.bind(this));
+	};
+
+	$p._loadYTScript = function(callback) {
+		Player.loadYTScript(callback);
+	};
+
+
+
+	return;
+
+	var elPlayer, videoId;
 	var url = 'https://www.youtube.com/iframe_api';
 	var elScript = document.createElement('script');
 	elScript.src = url;
