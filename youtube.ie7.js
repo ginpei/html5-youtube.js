78e8695b1096572a38bf2b64c1bebe58ce3ca3b3
ginpei <ginpei@gmail.com>
2015-04-14 17:17:27 -0700

    fix a problem that binded function will get arguments without expanded

--- youtube.ie7.js
+++ youtube.ie7.js
@@ -1,7 +1,7 @@
 window.youtube.bind = function(fn, context) {
 	var args = Array.prototype.slice.call(arguments, 2);
 	return function() {
-		var curArgs = args.concat(arguments);
+		var curArgs = args.concat(Array.prototype.slice.call(arguments));
 		return fn.apply(context, curArgs);
 	};
 };
