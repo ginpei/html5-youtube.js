fba3aab0ec54da4c631abce9ab54f1effd19964e
ginpei <ginpei@gmail.com>
2015-04-29 21:39:32 -0700

    basic test

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
-describe('Statics', function() {
-	it('is a function', function() {
-		expect(youtube.Player instanceof Function).toBeTruthy();
+var Player = window.youtube.Player;
+
+var player;
+var elParent;
+var elPlayer;
+var videoId;
+
+beforeEach(function(options) {
+	elParent = document.createElement('div');
+	elPlayer = document.createElement('div');
+	videoId = '123';
+
+	elParent.appendChild(elPlayer);
+
+	options = options || {};
+	options.el = options.el || elPlayer;
+	options.id = options.id || videoId;
+
+	player = new Player(options);
+});
+
+describe('Constructing', function() {
+	describe('instance', function() {
+		it('is a instance', function() {
+			expect(player instanceof Player).toBeTruthy();
+		});
 	});
 });
