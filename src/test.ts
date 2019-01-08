415c897f6417eaef182c9b450c150d2d8db21a87
ginpei <ginpei@gmail.com>
2019-01-08 11:36:55 -0800

    lint

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,4 @@
-// tslint:disable
-import Html5YouTubeOriginal, { PlayerState, IOptions } from './Html5YouTube';
+import Html5YouTubeOriginal, { IOptions, PlayerState } from './Html5YouTube';
 
 class Html5YouTube extends Html5YouTubeOriginal {
   public _buildPlayer (options: IOptions) {
@@ -7,19 +6,15 @@ class Html5YouTube extends Html5YouTubeOriginal {
   }
 
   public _createPlayer (el: HTMLElement, options: YT.PlayerOptions) {
-    var instance = this;
     const ytPlayer: any = {
-      playVideo: function() {
-        instance.onStateChange({ data: PlayerState.PLAYING });
-      },
-      seekTo: function() {},
-      setVolume: function() {},
-      mute: function() {},
-      unMute: function() {},
-      setPlaybackRate: function() {},
-      cueVideoById: function() {},
-      destroy: function() {
-      }
+      cueVideoById: () => undefined,
+      destroy: () => undefined,
+      mute: () => undefined,
+      playVideo: () => this.onStateChange({ data: PlayerState.PLAYING }),
+      seekTo: () => undefined,
+      setPlaybackRate: () => undefined,
+      setVolume: () => undefined,
+      unMute: () => undefined,
     };
     return ytPlayer;
   }
@@ -29,284 +24,272 @@ let player: Html5YouTube;
 let elParent: HTMLElement;
 let elPlayer: HTMLElement;
 let videoId: string;
-const originalStatics: { [key: string]: any } = {};
 
-for (var key in Html5YouTube) {
-	originalStatics[key] = Html5YouTube[key];
-}
+beforeEach(() => {
+  // create materials
+  elParent = document.createElement('div');
+  elPlayer = document.createElement('div');
+  videoId = '123';
 
-beforeEach(function() {
-	// restore statics
-	for (var name in Html5YouTube) {
-		delete Html5YouTube[name];
-	}
-	for (var name in originalStatics) {
-		Html5YouTube[name] = originalStatics[name];
-	}
-
-	// create materials
-	elParent = document.createElement('div');
-	elPlayer = document.createElement('div');
-	videoId = '123';
-
-	elParent.appendChild(elPlayer);
+  elParent.appendChild(elPlayer);
 
   // build an instance
-	player = new Html5YouTube({
-		el: elPlayer,
-		id: videoId
+  player = new Html5YouTube({
+    el: elPlayer,
+    id: videoId,
   });
 });
 
-afterEach(function() {
-	player.destroy();
+afterEach(() => {
+  player.destroy();
 });
 
-describe('Statics', function() {
-	describe('interface', function() {
-		it('builds new instance', function() {
-			var player = new Html5YouTube({ el:elPlayer, id:videoId });
-			expect(player instanceof Html5YouTube).toBeTruthy();
-		});
-	});
+describe('Statics', () => {
+  describe('interface', () => {
+    it('builds new instance', () => {
+      const instance = new Html5YouTube({ el:elPlayer, id:videoId });
+      expect(instance instanceof Html5YouTube).toBeTruthy();
+    });
+  });
 });
 
-describe('Constructing', function() {
-  it('loads the video by specified ID from the beginning', function() {
+describe('Constructing', () => {
+  it('loads the video by specified ID from the beginning', () => {
     player.destroy();
 
-    var result;
+    let result;
     const _createPlayer = Html5YouTube.prototype._createPlayer;
-  	Html5YouTube.prototype._createPlayer = function(el, options) {
-  		result = options.videoId;
-  	};
+    Html5YouTube.prototype._createPlayer = (el, options) => {
+      result = options.videoId;
+    };
     player = new Html5YouTube({ el:elPlayer, id:videoId });
     Html5YouTube.prototype._createPlayer = _createPlayer;
 
-  	expect(result).toBe(videoId);
+    expect(result).toBe(videoId);
   });
 
-	describe('instance', function() {
-		it('is an instance', function() {
-			expect(player instanceof Html5YouTube).toBeTruthy();
-		});
-	});
+  describe('instance', () => {
+    it('is an instance', () => {
+      expect(player instanceof Html5YouTube).toBeTruthy();
+    });
+  });
 
-	describe('video options', function() {
-		it('has videoId if ID is specified as a data attribute on the element', function() {
-			elPlayer.setAttribute('data-youtube-videoid', videoId);
-			var videoOptions = player._getVideoOptions({ el:elPlayer });
-			expect(videoOptions.videoId).toBe(videoId);
-		});
+  describe('video options', () => {
+    it('has videoId if ID is specified as a data attribute on the element', () => {
+      elPlayer.setAttribute('data-youtube-videoid', videoId);
+      const videoOptions = player._getVideoOptions({ el:elPlayer });
+      expect(videoOptions.videoId).toBe(videoId);
+    });
 
-		describe('playerVars settings', function() {
-			it('uses the value specified in options if specified on the element', function() {
-				elPlayer.setAttribute('data-youtube-controls', '1');
-				var videoOptions = player._getVideoOptions({ el:elPlayer, controls:0 });
-				expect(videoOptions.playerVars.controls).toBe(0);
-			});
+    describe('playerVars settings', () => {
+      it('uses the value specified in options if specified on the element', () => {
+        elPlayer.setAttribute('data-youtube-controls', '1');
+        const videoOptions = player._getVideoOptions({ el:elPlayer, controls:0 });
+        expect(videoOptions.playerVars.controls).toBe(0);
+      });
 
-			it('let youtube fallback to default settings if not specified', function() {
-				elPlayer.removeAttribute('data-youtube-controls');
-				var videoOptions = player._getVideoOptions({ el:elPlayer });
-				expect(videoOptions.playerVars.controls).toBe(undefined);
-			});
+      it('let youtube fallback to default settings if not specified', () => {
+        elPlayer.removeAttribute('data-youtube-controls');
+        const videoOptions = player._getVideoOptions({ el:elPlayer });
+        expect(videoOptions.playerVars.controls).toBe(undefined);
+      });
 
-			it('turns setting on if "true" is specified', function() {
-				elPlayer.setAttribute('data-youtube-controls', 'true');
-				var videoOptions = player._getVideoOptions({ el:elPlayer });
-				expect(videoOptions.playerVars.controls).toBe(1);
-			});
+      it('turns setting on if "true" is specified', () => {
+        elPlayer.setAttribute('data-youtube-controls', 'true');
+        const videoOptions = player._getVideoOptions({ el:elPlayer });
+        expect(videoOptions.playerVars.controls).toBe(1);
+      });
 
-			it('let youtube fallback to default settings if invalid number like "-1" is specified', function() {
-				elPlayer.setAttribute('data-youtube-controls', '-1');
-				var videoOptions = player._getVideoOptions({ el:elPlayer });
-				expect(videoOptions.playerVars.controls).toBe(undefined);
-			});
+      it('let youtube fallback to default settings if invalid number like "-1" is specified', () => {
+        elPlayer.setAttribute('data-youtube-controls', '-1');
+        const videoOptions = player._getVideoOptions({ el:elPlayer });
+        expect(videoOptions.playerVars.controls).toBe(undefined);
+      });
 
-			it('turns setting off if "false" is specified', function() {
-				elPlayer.setAttribute('data-youtube-controls', 'false');
-				var videoOptions = player._getVideoOptions({ el:elPlayer });
-				expect(videoOptions.playerVars.controls).toBe(0);
-			});
+      it('turns setting off if "false" is specified', () => {
+        elPlayer.setAttribute('data-youtube-controls', 'false');
+        const videoOptions = player._getVideoOptions({ el:elPlayer });
+        expect(videoOptions.playerVars.controls).toBe(0);
+      });
 
-			it('accepts strings like "playlist" as valid values', function() {
-				elPlayer.setAttribute('data-youtube-listType', 'playlist');
-				var videoOptions = player._getVideoOptions({ el:elPlayer });
-				expect(videoOptions.playerVars.listType).toBe('playlist');
-			});
+      it('accepts strings like "playlist" as valid values', () => {
+        elPlayer.setAttribute('data-youtube-listType', 'playlist');
+        const videoOptions = player._getVideoOptions({ el:elPlayer });
+        expect(videoOptions.playerVars.listType).toBe('playlist');
+      });
 
-			it('allows youtube playerVars settings as booleans', function() {
-				elPlayer.removeAttribute('data-youtube-controls');
-				var videoOptions = player._getVideoOptions({
-					el:elPlayer,
-					autohide: false,
-					autoplay: true,
-					cc_load_policy: true,
-					color: 'white',
-					controls: false,
-					disablekb: false,
-					enablejsapi: true,
-					end: 23,
-					fs: false,
-					hl: 'en',
-					iv_load_policy: 3,
-					list: 'PLC77007E23FF423C6',
-					listType: 'playlist',
-					loop: false,
-					modestbranding: true,
-					origin: 'localhost',
-					playerapiid: '1234abcd',
-					playlist: '2EEsa_pqGAs,KFstP0C9sVk',
-					playsinline: true,
-					rel: false,
-					showinfo: false
-				});
+      it('allows youtube playerVars settings as booleans', () => {
+        elPlayer.removeAttribute('data-youtube-controls');
+        const videoOptions = player._getVideoOptions({
+          autohide: false,
+          autoplay: true,
+          cc_load_policy: true,
+          color: 'white',
+          controls: false,
+          disablekb: false,
+          el:elPlayer,
+          enablejsapi: true,
+          end: 23,
+          fs: false,
+          hl: 'en',
+          iv_load_policy: 3,
+          list: 'PLC77007E23FF423C6',
+          listType: 'playlist',
+          loop: false,
+          modestbranding: true,
+          origin: 'localhost',
+          playerapiid: '1234abcd',
+          playlist: '2EEsa_pqGAs,KFstP0C9sVk',
+          playsinline: true,
+          rel: false,
+          showinfo: false,
+        });
 
-				expect(videoOptions.playerVars).toEqual({
-					autohide: 0,
-					autoplay: 1,
-					cc_load_policy: 1,
-					color: 'white',
-					controls: 0,
-					disablekb: 0,
-					enablejsapi: 1,
-					end: 23,
-					fs: 0,
-					hl: 'en',
-					iv_load_policy: 3,
-					list: 'PLC77007E23FF423C6',
-					listType: 'playlist',
-					loop: 0,
-					modestbranding: 1,
-					origin: 'localhost',
-					playerapiid: '1234abcd',
-					playlist: '2EEsa_pqGAs,KFstP0C9sVk',
-					playsinline: 1,
-					rel: 0,
-					showinfo: 0,
-					start: undefined,
-					theme: undefined
-				});
-			});
+        expect(videoOptions.playerVars).toEqual({
+          autohide: 0,
+          autoplay: 1,
+          cc_load_policy: 1,
+          color: 'white',
+          controls: 0,
+          disablekb: 0,
+          enablejsapi: 1,
+          end: 23,
+          fs: 0,
+          hl: 'en',
+          iv_load_policy: 3,
+          list: 'PLC77007E23FF423C6',
+          listType: 'playlist',
+          loop: 0,
+          modestbranding: 1,
+          origin: 'localhost',
+          playerapiid: '1234abcd',
+          playlist: '2EEsa_pqGAs,KFstP0C9sVk',
+          playsinline: 1,
+          rel: 0,
+          showinfo: 0,
+          start: undefined,
+          theme: undefined,
+        });
+      });
 
-			it('allows youtube playerVars settings as numbers', function() {
-				var videoOptions = player._getVideoOptions({
-					el:elPlayer,
-					autohide: 0,
-					autoplay: 1,
-					cc_load_policy: 1,
-					color: 'white',
-					controls: 0,
-					disablekb: 0,
-					enablejsapi: 1,
-					end: 23,
-					fs: 0,
-					hl: 'en',
-					iv_load_policy: 3,
-					list: 'PLC77007E23FF423C6',
-					listType: 'playlist',
-					loop: 0,
-					modestbranding: 1,
-					origin: 'localhost',
-					playerapiid: '1234abcd',
-					playlist: '2EEsa_pqGAs,KFstP0C9sVk',
-					playsinline: 1,
-					rel: 0,
-					showinfo: 0,
-					start: 10,
-					theme: 'light'
-				});
+      it('allows youtube playerVars settings as numbers', () => {
+        const videoOptions = player._getVideoOptions({
+          autohide: 0,
+          autoplay: 1,
+          cc_load_policy: 1,
+          color: 'white',
+          controls: 0,
+          disablekb: 0,
+          el:elPlayer,
+          enablejsapi: 1,
+          end: 23,
+          fs: 0,
+          hl: 'en',
+          iv_load_policy: 3,
+          list: 'PLC77007E23FF423C6',
+          listType: 'playlist',
+          loop: 0,
+          modestbranding: 1,
+          origin: 'localhost',
+          playerapiid: '1234abcd',
+          playlist: '2EEsa_pqGAs,KFstP0C9sVk',
+          playsinline: 1,
+          rel: 0,
+          showinfo: 0,
+          start: 10,
+          theme: 'light',
+        });
 
-				expect(videoOptions.playerVars).toEqual({
-					autohide: 0,
-					autoplay: 1,
-					cc_load_policy: 1,
-					color: 'white',
-					controls: 0,
-					disablekb: 0,
-					enablejsapi: 1,
-					end: 23,
-					fs: 0,
-					hl: 'en',
-					iv_load_policy: 3,
-					list: 'PLC77007E23FF423C6',
-					listType: 'playlist',
-					loop: 0,
-					modestbranding: 1,
-					origin: 'localhost',
-					playerapiid: '1234abcd',
-					playlist: '2EEsa_pqGAs,KFstP0C9sVk',
-					playsinline: 1,
-					rel: 0,
-					showinfo: 0,
-					start: 10,
-					theme: 'light'
-				});
-			});
-		});
-	});
+        expect(videoOptions.playerVars).toEqual({
+          autohide: 0,
+          autoplay: 1,
+          cc_load_policy: 1,
+          color: 'white',
+          controls: 0,
+          disablekb: 0,
+          enablejsapi: 1,
+          end: 23,
+          fs: 0,
+          hl: 'en',
+          iv_load_policy: 3,
+          list: 'PLC77007E23FF423C6',
+          listType: 'playlist',
+          loop: 0,
+          modestbranding: 1,
+          origin: 'localhost',
+          playerapiid: '1234abcd',
+          playlist: '2EEsa_pqGAs,KFstP0C9sVk',
+          playsinline: 1,
+          rel: 0,
+          showinfo: 0,
+          start: 10,
+          theme: 'light',
+        });
+      });
+    });
+  });
 
-	describe('src', function() {
-		var videoId;
-		beforeEach(function() {
-			videoId = 'videoId' + Date.now();
-		});
+  describe('src', () => {
+    let videoId2; // find out better name
+    beforeEach(() => {
+      videoId2 = 'videoId' + Date.now();
+    });
 
-		it('loads the video by ID that is set as `src` after ready event', function() {
-			var ytPlayer = player.player;
-			ytPlayer.cueVideoById = function(value) {
-				this._src = value;
-			};
-			ytPlayer.getVideoUrl = function() {
-				return this._src;
-			};
+    it('loads the video by ID that is set as `src` after ready event', () => {
+      const ytPlayer = player.player;
+      ytPlayer.cueVideoById = function (value) {
+        this._src = value;
+      };
+      ytPlayer.getVideoUrl = function () {
+        return this._src;
+      };
 
-			player.player = null;
-			player.src = videoId;
+      player.player = null;
+      player.src = videoId2;
 
-			player.player = ytPlayer;
-			player.onReady(null);
+      player.player = ytPlayer;
+      player.onReady(null);
 
-			expect(player.src).toBe(videoId);
-		});
-	});
+      expect(player.src).toBe(videoId2);
+    });
+  });
 });
 
-describe('Events', function() {
-	describe('on()', function() {
-		it('adds a listener', function() {
-			var called = 0;
-			player.on('play', function() {
-				called++;
+describe('Events', () => {
+  describe('on()', () => {
+    it('adds a listener', () => {
+      let called = 0;
+      player.on('play', () => {
+        called++;
       });
-			player.play();
-			expect(called).toBe(1);
-		});
+      player.play();
+      expect(called).toBe(1);
+    });
 
-		it('returns its own', function() {
-			expect(player.on('', function(){})).toBe(player);
-		});
-	});
+    it('returns its own', () => {
+      expect(player.on('', () => undefined)).toBe(player);
+    });
+  });
 
-	describe('off()', function() {
-		var called, listener;
-		beforeEach(function() {
-			called = 0;
-			listener = function() {
-				called++;
-			};
-			player.on('play', listener);
-		});
+  describe('off()', () => {
+    let called;
+    let listener;
+    beforeEach(() => {
+      called = 0;
+      listener = () => {
+        called++;
+      };
+      player.on('play', listener);
+    });
 
-		it('removes a listener', function() {
-			player.off('play', listener);
-			player.play();
-			expect(called).toBe(0);
-		});
+    it('removes a listener', () => {
+      player.off('play', listener);
+      player.play();
+      expect(called).toBe(0);
+    });
 
-		it('returns its own', function() {
-			expect(player.off('play', listener)).toBe(player);
-		});
-	});
+    it('returns its own', () => {
+      expect(player.off('play', listener)).toBe(player);
+    });
+  });
 });
