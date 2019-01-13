c2b6d6edf3bafcd16aa6aa9ae6f0393ecad37488
ginpei <ginpei@gmail.com>
2019-01-12 19:46:31 -0800

    follow renamed file

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
   mode: 'development',
   entry: {
     'web/html5-youtube': './src/web.ts',
-    'test': './src/test.ts',
+    'test': './src/Html5YouTube.test.ts',
   },
   devtool: 'inline-source-map',
   devServer: {
