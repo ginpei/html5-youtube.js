359f9931420518a32f45992cd666f5532e6cb0aa
ginpei <ginpei@gmail.com>
2019-01-14 10:16:21 -0800

    use new interfaces

--- src/web.ts
+++ src/web.ts
@@ -1,15 +1,11 @@
-import Html5YouTube, { IOptions, PlayerState } from './Html5YouTube';
+import Html5YouTube, { PlayerState } from './Html5YouTube';
 
 declare global {
   // tslint:disable-next-line:interface-name
   interface Window {
     Html5YouTube: any;
-    youtube: (options: IOptions) => Html5YouTube;
   }
 }
 
 window.Html5YouTube = Html5YouTube;
 window.Html5YouTube.PlayerState = PlayerState;
-window.youtube = (options: IOptions) => {
-  return new Html5YouTube(options);
-};
